b2af8e99e1c6dc7f1b5e2811a6ff80d207e69bb7
MunoLike <sukorice@gmail.com>
2025-01-19 02:40:45 +0900

    https://github.com/MoriKen254/Control-System-DesignGetting-Started-With-Arduino-and-MATLAB これのままやってみる

--- velo_id_gain.m
+++ velo_id_gain.m
@@ -1,71 +1,77 @@
 %% velo_id_gain.m
 
-%% initialize
+%% Initialize
 clear all
 close all
 
-%% parameters
-ts = 1/50;
-u_ini = 1.5; 
+%% Parameters
+ts  = 1/50;
+u_ini = 1.5;
 
-%% define input voltage list
-u_ref_list = 0.5:0.25:2;
+%% Define input voltage list
+u_ref_list = 1:0.125:2.5;
 
-%% open simulink model
+%% Open simulink model
 open_system('velo_id_gain_sl');
-open_system('velo_id_gain_sl/Output');
+open_system('velo_id_gain_sl/Out1');
 
-%% parameter identification
-y_mean_list = [];
-for i = 1:length(u_ref_list)
+%% Start experiment
+y_mean_list= [];
+for i=1:length(u_ref_list)
     u_ref = u_ref_list(i);
+    %    sim('velo_id_gain_sl');
     set_param('velo_id_gain_sl', 'SimulationCommand', 'start')
-    %% wait
+    while 1
+        status = get_param('velo_id_gain_sl', 'SimulationStatus');
+        if(strcmp(status, 'stopped'))
+            break;
+        end
+    end
     t = yout.time;
-    y = yout.signals.values(1, 1, :);
+    y = yout.signals.values;
     y_mean = mean(y(250:end));
-    fprintf('y_mean = %f\n', y_mean);
+    fprintf('y_mean = %f\n', y_mean) ;
     y_mean_list(i) = y_mean;
-    % plot figure
+    % Plot figure
     figure(1)
     plot(t, y, t, ones(size(y))*y_mean,'r');
-    xlabel('Time [s]'), ylabel('Velocity [V]')
+    xlabel ('Time [s]'), ylabel ('Velocity [V]')
     axis([0 10 0 3])
     drawnow
 end
 
-%% plot data
+%% Plot data
 figure(2)
 plot(u_ref_list, y_mean_list, 'bo')
 xlabel('Input voltage [V]')
 ylabel('Velocity [V]')
 axis([0 3 0 3])
 
-%% calculate parameters
+%% Calculate parameters
 while(1)
-    disp('please input Umin and Umax for fit')
+    disp('Please  input Umin and Umax for fit')
     umin = input('Umin = ');
     umax = input('Umax = ');
     sidx = min(find(u_ref_list >= umin));
-    eidx = min(find(u_ref_list <= umin));
-    P = polyfit(u_ref_list(sidx:eidx), y_mean_list(sidx:eidx), 1);
-    Pin = 0:3;
-    Pout = P(1) * Pin + P(2);
-    u_offset = -P(2) / P(1);
-    fprintf('***Motor parameters***\n')
-    fprintf('K        = %f\n', P(1))
-    fprintf('u_offset = %f\n', u_offset)
-
+    eidx  =max(find(u_ref_list <= umax));
+    P = polyfit(u_ref_list(sidx:eidx),y_mean_list(sidx:eidx), 1) ;
+    Pin  = 0:3;
+    Pout = P(1)* Pin + P(2);
+    u_offset = -P(2)/P(1);
+    fprintf('*** Motor parameters * ***\n')
+    fprintf('K        = %f\n', P(1));
+    fprintf('u_offset = %f\n', u_offset);
+    
     figure(2)
-    plot(u_ref_list, y_mean_list, 'o', ...
-        u_ref_list(sidx:eidx), y_mean_list(sidx:eidx), 'ro', ...
+    plot(u_ref_list, y_mean_list, 'o',...
+        u_ref_list(sidx:eidx),y_mean_list(sidx:eidx), 'ro',...
         Pin, Pout, 'r-')
-    Xlabel('Input voltage [V]')
+    xlabel('Input voltage [V]')
     ylabel('Velocity [V]')
     axis([0 3 0 3])
-
-    sw = input('OK? (1:Quit, 2:Retry) = ');
-    switch sw
+    
+    ww = input('DK? (1:Quit, 2:Retry) = ');
+    switch ww
         case 1;
             break;
         case 2;
@@ -73,4 +79,4 @@ while(1)
     end
 end
 
-%% EOF of velo_id_gain.m
+%% EDF of velo_id_gain.m
